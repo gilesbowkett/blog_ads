2ea97b16f36254e05f26988e31cfd01d78398124
Giles Bowkett <gilesb@gmail.com>
2009-11-01 08:33:18 -0800

    plural @ads, not singular @ad
    dummy code in generator, ERB looks finished tho, for the moment

--- generator.rb
+++ generator.rb
@@ -14,10 +14,17 @@ class Ad
   end
 end
 
-@ad = Ad.new
-@ad.title = "Kermit The Frog"
-@ad.text = "Played with style and aplomb by the incomparable Elizabeth 'Tina' Fey"
-@ad.affiliate = "http://giles.30rock.com"
-@ad.pretty = "http://30rock.com"
+
+@ads = []
+
+5.times do
+  ad = Ad.new
+  ad.title = "Kermit The Frog"
+  ad.text = "Played with style and aplomb by the incomparable Elizabeth 'Tina' Fey"
+  ad.affiliate = "http://giles.30rock.com"
+  ad.pretty = "http://30rock.com"
+
+  @ads << ad
+end
 
 puts ERB.new(File.read(TEMPLATE)).result(binding)
