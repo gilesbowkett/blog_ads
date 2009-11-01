fa545eb4f153b4f7982324047514ef6bf2c30eb7
Giles Bowkett <gilesb@gmail.com>
2009-11-01 08:58:22 -0800

    cheesy data
    OK, I looked at doing this with ActiveRecord, but I wanted it done as
    quickly as possible. moving this to ARec is probably the way to go - in
    fact part of me just wants to replace it with a Rails app, so I get the
    console, but you can get the console so easy that it doesnt' matter.
    
    anyway, for now, it's a flat file. easy enough to change it later, but
    YAGNI.

--- generator.rb
+++ generator.rb
@@ -17,14 +17,35 @@ end
 
 @ads = []
 
-5.times do
-  ad = Ad.new
-  ad.title = "Kermit The Frog"
-  ad.text = "Played with style and aplomb by the incomparable Elizabeth 'Tina' Fey"
-  ad.affiliate = "http://giles.30rock.com"
-  ad.pretty = "http://30rock.com"
+@ads[0] = Ad.new
+@ads[0].title = "Kermit The Frog"
+@ads[0].text = "Played with style and aplomb by the incomparable Elizabeth 'Tina' Fey"
+@ads[0].affiliate = "http://giles.30rock.com"
+@ads[0].pretty = "http://30rock.com"
 
-  @ads << ad
-end
+@ads[1] = Ad.new
+@ads[1].title = "Kermit The Frog"
+@ads[1].text = "Played with style and aplomb by the incomparable Elizabeth 'Tina' Fey"
+@ads[1].affiliate = "http://giles.30rock.com"
+@ads[1].pretty = "http://30rock.com"
+
+@ads[2] = Ad.new
+@ads[2].title = "Kermit The Frog"
+@ads[2].text = "Played with style and aplomb by the incomparable Elizabeth 'Tina' Fey"
+@ads[2].affiliate = "http://giles.30rock.com"
+@ads[2].pretty = "http://30rock.com"
+
+@ads[3] = Ad.new
+@ads[3].title = "Kermit The Frog"
+@ads[3].text = "Played with style and aplomb by the incomparable Elizabeth 'Tina' Fey"
+@ads[3].affiliate = "http://giles.30rock.com"
+@ads[3].pretty = "http://30rock.com"
+
+@ads[4] = Ad.new
+@ads[4].title = "Kermit The Frog"
+@ads[4].text = "Played with style and aplomb by the incomparable Elizabeth 'Tina' Fey"
+@ads[4].affiliate = "http://giles.30rock.com"
+@ads[4].pretty = "http://30rock.com"
+
 
 puts ERB.new(File.read(TEMPLATE)).result(binding)
